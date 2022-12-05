d9758c79585565205c02149318670607b1af6458
Juand30 <juandp2290@gmail.com>
2022-12-05 21:00:11 +0000

    Inicio proyecto

---
+++ src/front/js/pages/login.js
@@ -0,0 +1,56 @@
+import React, { useContext, useState } from "react";
+import { Context } from "../store/appContext";
+import rigoImageUrl from "../../img/rigo-baby.jpg";
+import "../../styles/home.css";
+
+export const Login = () => {
+  const { store, actions } = useContext(Context);
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const handleClick = () => {
+    const opts = {
+      method: "POST",
+      Headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        email: email,
+        password: password,
+      }),
+    };
+    fetch(
+      "https://3000-juand30-flaskproyect-67g3ials120.ws-eu77.gitpod.io/api/token",
+      opts
+    )
+      .then((resp) => {
+        if (resp.status === 200) return resp.json();
+        else alert("Hay algun error");
+      })
+      .then()
+      .catch((error) => {
+        console.error("Esto es un error de password o user", error);
+      });
+  };
+
+  return (
+    <div className="text-center mt-5">
+      <h1>Login</h1>
+      <div>
+        <input
+          type="text"
+          placeholder="E-Mail"
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+        />
+        <input
+          type="password"
+          placeholder="Password"
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+        />
+        <button onClick={handleClick}>Login</button>
+      </div>
+    </div>
+  );
+};
